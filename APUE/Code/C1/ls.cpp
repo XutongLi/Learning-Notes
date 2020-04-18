a309ca47eb4cfabe560a3a2eeb39d8b659e48f4c
XutongLi <brianleelxt@gmail.com>
2020-04-19 06:27:45 +0800

    init apue

---
+++ APUE/Code/C1/ls.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+    > File Name: ls.cpp
+    > Author: lixutong
+    > Created Time: 2020-04-19 05:51:04
+ ************************************************************************/
+
+#include "../include/apue.h"
+#include <iostream>
+#include <dirent.h>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+    DIR *dp;
+    struct dirent *dirp;
+
+    if (argc != 2)
+        cout << "usage: ls direction_name" << endl;
+
+    if ((dp = opendir(argv[1])) == nullptr)
+        cout << "Can't open %s" << argv[1] << endl;
+    while ((dirp = readdir(dp)) != nullptr)
+        cout << dirp->d_name << endl;
+
+    closedir(dp);
+    exit(0);
+}
+
---
+++ APUE/Code/hello/main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: lixutong
+    > Created Time: 2020-04-19 05:37:06
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
+
