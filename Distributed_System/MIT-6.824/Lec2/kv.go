42375b5296977592558ec7e8005caff5aee554f9
XutongLi <brianleelxt@gmail.com>
2020-05-31 06:59:46 +0800

    add MIT-6.824 Lec2 FAQ

--- Distributed_System/MIT-6.824/Lec2/kv.go
+++ Distributed_System/MIT-6.824/Lec2/kv.go
@@ -42,7 +42,7 @@ type GetReply struct {		// get操作返回值
 //
 
 func connect() *rpc.Client {
-	client, err := rpc.Dial("tcp", ":1234")
+	client, err := rpc.Dial("tcp", "127.0.0.1:1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
@@ -85,8 +85,8 @@ func server() {
 	kv := new(KV)
 	kv.data = map[string]string{}
 	rpcs := rpc.NewServer()
-	rpcs.Register(kv)
-	l, e := net.Listen("tcp", ":1234")
+	rpcs.Register(kv)	//一种类型只能注册一个
+	l, e := net.Listen("tcp", "127.0.0.1:1234")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
